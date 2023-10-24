b84ac12770229d2d1a0cfbbd54f76b226f653842
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-10-24 10:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your code here
+const container = document.getElementById("container");
+const images = document.getElementsByClassName("image");
+
+		  function onDragStart(event) {
+  // event.target represents the dragged element
+  event.dataTransfer.setData("sourceId", event.target.id);
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function onDrop(event) {
+  // event.target => on which we drop some element
+  const sourceId = event.dataTransfer.getData("sourceId");
+  const sourceElement = document.getElementById(sourceId);
+  const destElement = event.target;
+
+  const sourceNextElement = sourceElement.nextElementSibling;
+  const destNextElement = destElement.nextElementSibling;
+
+  // adding destElement in front of sourceNextElement
+  container.insertBefore(destElement, sourceNextElement);
+
+  // adding sourceElement in front of destNextElement
+  container.insertBefore(sourceElement, destNextElement);
+}
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("dragstart", onDragStart);
+  images[i].addEventListener("dragover", onDragOver);
+  images[i].addEventListener("drop", onDrop);
+}
